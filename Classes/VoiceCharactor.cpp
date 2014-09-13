6ae0586d0eb1a72eb271a613b62c0fbfdf9d4187
jun <master@192.168.11.9>
2014-09-13 16:40:56 +0900

    Macプロジェクト調整
    ファイル調整

---
+++ Classes/VoiceCharactor.cpp
@@ -0,0 +1,9 @@
+//
+//  VoiceCharactor.cpp
+//  7th_Voice
+//
+//  Created by 王純 on 2014/09/13.
+//
+//
+
+#include "VoiceCharactor.h"
---
+++ Classes/VoiceCharactor.h
@@ -0,0 +1,14 @@
+//
+//  VoiceCharactor.h
+//  7th_Voice
+//
+//  Created by 王純 on 2014/09/13.
+//
+//
+
+#ifndef ___th_Voice__VoiceCharactor__
+#define ___th_Voice__VoiceCharactor__
+
+#include <iostream>
+
+#endif /* defined(___th_Voice__VoiceCharactor__) */
